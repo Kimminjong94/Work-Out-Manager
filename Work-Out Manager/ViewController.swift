55fa18b5010579ddd73433f324928f8d79275d4b
kimminjong <easyletgothen@gmail.com>
2022-09-18 18:54:19 +0900

    Initial Commit

---
+++ Work-Out Manager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Work-Out Manager
+//
+//  Created by 김민종 on 2022/09/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
